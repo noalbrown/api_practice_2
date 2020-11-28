0daafe295094f93b724b4e958d4ceedd0aa87cc2
Noal Brown <noalbrown1@gmail.com>
2020-11-28 10:40:47 -0600

    changes

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,24 +1,21 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 
 
-export class Navbar extends Component {
-
-  static defaultProps = {
-    title: 'Github Finder'
-  };
-
-  static propTypes = {
-    title: PropTypes.string.isRequired
-  }
-
-  render() {
-    return (
-      <nav className='navbar bg-primary'>
-        <h1>{this.props.title}</h1>
-      </nav>
-    );
-  }
+const Navbar = (props) => {
+  return (
+    <nav className='navbar bg-primary'>
+      <h1>{props.title}</h1>
+    </nav>
+  );
 };
+
+Navbar.defaultProps = {
+  title: 'Github Finder'
+};
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired
+}
 
 export default Navbar;
